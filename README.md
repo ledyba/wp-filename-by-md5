7ecc2537727e5deef1f142e50bf0e72d20eac375
psi <psi@7io.org>
2018-05-02 18:26:43 +0900

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# wp-filename-by-md5
+
+ Hashify uploaded filenames from the md5 hashes of *** file content ***.
+
+ All other plugins that say "renaming filename by hash" DO NOT consider about content. They use just a hash of FILENAME. I can't believe it.
---
+++ plugin.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Plugin Name: wp-filename-by-md5
+ * Plugin URI: https://github.com/ledyba/wp-filename-by-md5
+ * Description: Hashify filename from content
+ * Author: PSI
+ * Author URI: https://7io.org
+ * Version: 1.0.0
+ * GitHub Plugin URL: ledyba/wp-filename-by-md5
+ *
+ */
+
+if( !function_exists( 'wp_rename_filename_by_md5' ) ) {
+  function wp_rename_filename_by_md5( $file ) {
+    // @See also: 
+    // $file: http://php.net/manual/en/reserved.variables.files.php#89674
+    $info = pathinfo($file['name']);
+    $ext  = empty($info['extension']) ? '' : '.' . $info['extension'];
+    $hash = md5_file($file['tmp_name'], false);
+    $file['name'] = $hash . $ext;
+    return $file;
+  }
+}
+add_filter( 'wp_handle_upload_prefilter', 'wp_rename_filename_by_md5', 10 );
